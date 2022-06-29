df331c6591d03f13e209231b0d97a372df853326
Anirudh-rao <anirudh131299@gmail.com>
2022-06-29 12:10:08 +0530

    Created Routes
    Added Project Routes to the server file

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module githu.com/Anirudh-rao/VideoCoferencingUsingGo
 
 go 1.18
+
+require github.com/googollee/go-socket.io v1.6.2
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,37 @@
 package server
+
+import (
+	"flag"
+	"os"
+
+	"githu.com/Anirudh-rao/VideoCoferencingUsingGo/internal/handlers"
+	"github.com/googollee/go-socket.io/engineio/transport/websocket"
+)
+
+var (
+	addr =  flag.String("addr,":"", os.Getenv("PORT"),"")
+	cert =  flag.String("cert","","")
+	key =  flag.String("key","","")
+)
+func Run() error{
+	flag.Parse()
+	if *addr = ":"{
+		*addr  = ":8080"
+	}
+	//Routes
+	app.GET("/", handlers.Welcome)
+	app.GET("/room/create", handlers.RoomCreate)
+	app.GET("/room/:uuid", handlers.Room)
+	app.GET("/room/:uuid/websocket", )
+	app.GET("/room/:uuid/chat", handlers.RoomChat)
+	app.GET("/room/:uuid/chat/websocket", websocket.New(handlers.RooomChatWeocket))
+	app.GET("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
+	app.GET("/stream/:ssuid", handlers.Stream)
+	app.GET("/stream/:ssuid/websocket", )
+	app.GET("/stream/:ssuid/chat/websocket", )
+	app.GET("/stream/:ssuid/viewer/websocket", )
+}
+
+func main(){
+
+}
